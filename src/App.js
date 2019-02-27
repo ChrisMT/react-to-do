9191a60f13b48f48455c59d28a595f2b3cfe20fa
ChrisMT <cmthielman@gmail.com>
2019-02-27 12:53:49 -0500

    state-and-props: completed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todos: [
+        {description: 'walk the dog', isCompleted: true},
+        {description: 'wash dishes', isCompleted: false},
+        {description: 'go on bikeride', isCompleted: false}
+      ]
+    };
+  }
+
+
   render() {
     return (
       <div className="App">
         <ul>
-          <ToDo />
-          <ToDo />
+          {this.state.todos.map( (todo, index) =>
+            <ToDo key={ index }
+              description={ todo.description }
+              isCompleted={ todo.isCompleted } />
+            )}
         </ul>
-
       </div>
     );
   }
